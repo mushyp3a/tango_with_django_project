7c85867e895cec4fa06314b1f869aaa42713bf52
Mackenzie Trafford <3029077T@student.gla.ac.uk>
2026-02-10 17:00:42 +0000

    ch.4 Added templates, static and media files

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)

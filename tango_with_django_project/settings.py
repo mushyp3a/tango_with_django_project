6fa08e584cb9620fe136ca121cc26ee9f602cf21
Mackenzie Trafford <3029077T@student.gla.ac.uk>
2026-02-13 13:03:51 +0000

    ch.9 Added user authentication w/ restricted page

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -23,6 +23,8 @@ STATICFILES_DIRS = [STATIC_DIR,]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+LOGIN_URL = 'rango:login'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 

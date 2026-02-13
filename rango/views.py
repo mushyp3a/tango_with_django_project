880eb90b01cf5c1f7ad87828951306f3c03eb35e
Mackenzie Trafford <3029077T@student.gla.ac.uk>
2026-02-13 13:08:15 +0000

    ch.9 Exercises

--- rango/views.py
+++ rango/views.py
@@ -36,6 +36,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -48,6 +49,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form':form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
